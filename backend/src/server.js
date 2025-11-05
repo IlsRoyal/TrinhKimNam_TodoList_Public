41a4d5334622291370885b09c296592fe71480f4
Trinh Kim Nam <namils0912@gmail.com>
2025-11-05 10:19:07 +0700

    chuan bi deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,10 @@ import taskRoute from "./routes/tasksRouters.js";
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from "path";
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -12,9 +14,19 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"}));
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({ origin: "http://localhost:5173"}));
+}
 
-app.use("/api/tasks", taskRoute)
+app.use("/api/tasks", taskRoute);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
